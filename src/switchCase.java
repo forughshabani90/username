f213f2deb94d1164369c6a11e2c9a5ea679abcf3
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-25 01:27:42 -0700

    fm

--- src/switchCase.java
+++ src/switchCase.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class switchCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        string weekday = scanner.next();
+        switch (weekday) {
+            case "saturday":
+                System.out.println("شنبه");
+                break;
+            case "sunday":
+                System.out.println("یکشنبه");
+                break;
+            case "monday":
+                System.out.println("دوشنبه");
+                break;
+            case "tusday":
+                System.out.println("سه شنبه");
+                break;
+            case "wednesday":
+                break;
+            case "thrsday":
+                System.out.println("پنجشنبه");
+                break;
+            case "friday":
+                System.out.println("جمعه");
+                break;
+        }
+
+    }
 }
